2ba2414ae0bc370f8095c3330c29828542f18e70
soyrcom <rodolforiverado@gmail.com>
2019-05-05 16:44:19 -0400

    mend

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@
 var abrir = document.querySelector('#abrir');
 var modal = document.querySelector('#modal_contacto');
 var cerrar = document.querySelector('#close');
+var responsive = document.querySelector('#responsiveBtn');
+var menuInResponsive = document.querySelector('#menu');
 
+// Modal elements
 abrir.addEventListener('click', function() {
-    
     modal.style.display = 'block';
 });
 
 cerrar.addEventListener('click', function(){
     modal.style.display = 'none';
 });
+
+// Responsive menu
+
+
+
+responsive.addEventListener('click', function(){
+    menuInResponsive.style.opacity = '1';
+    
+});
+
+
+
+
